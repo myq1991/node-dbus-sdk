f940608ecde92f53372a23f4c5ab6ca7691ebc6e
myq1991 <myq1991@gmail.com>
2025-05-09 16:38:37 +0800

    feat(NodeDBus): add exports for DBus types and classes

--- src/NodeDBus.ts
+++ src/NodeDBus.ts
@@ -1,1 +1,8 @@
-//TODO
+export {type DBusServiceOpts} from './types/DBusServiceOpts'
+export {type DBusObjectOpts} from './types/DBusObjectOpts'
+export {type DBusInterfaceOpts} from './types/DBusInterfaceOpts'
+
+export {DBus} from './DBus'
+export {DBusService} from './DBusService'
+export {DBusObject} from './DBusObject'
+export {DBusInterface} from './DBusInterface'
