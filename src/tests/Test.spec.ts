fb5bbae71015f223db2556bbb9f3e83d19f9f5c4
myq1991 <myq1991@gmail.com>
2025-07-23 16:27:44 +0800

    test: update Test.spec.ts with DBus connection example Updated test file to demonstrate DBus connection and interaction with org.freedesktop.login1 service instead of local TCP setup.

--- src/tests/Test.spec.ts
+++ src/tests/Test.spec.ts
@@ -1,5 +1,6 @@
 import {SetupLocalServiceSpec} from './examples/SetupLocalService.spec'
 import {InvokeDBusServiceSpec} from './examples/InvokeDBusService.spec'
+import {DBus} from '../NodeDBusSDK'
 
 /**
  * Executes test specifications for DBus service setup and invocation.
@@ -7,10 +8,29 @@ import {InvokeDBusServiceSpec} from './examples/InvokeDBusService.spec'
  * demonstrating both server and client interactions over a TCP connection.
  * The operations are scheduled to run immediately in the next event loop cycle.
  */
+// setImmediate(async (): Promise<void> => {
+//     // Set up a local DBus service with the specified name and TCP bus address
+//     await SetupLocalServiceSpec('org.dbus.node.test', 'tcp:host=192.168.1.127,port=44444')
+//
+//     // Invoke the DBus service as a client to test interactions with the local service
+//     await InvokeDBusServiceSpec('org.dbus.node.test', 'tcp:host=192.168.1.127,port=44444')
+// })
+
 setImmediate(async (): Promise<void> => {
     // Set up a local DBus service with the specified name and TCP bus address
-    await SetupLocalServiceSpec('org.dbus.node.test', 'tcp:host=192.168.1.127,port=44444')
+    // await SetupLocalServiceSpec('org.dbus.node.test', 'tcp:host=192.168.1.127,port=44444')
 
     // Invoke the DBus service as a client to test interactions with the local service
-    await InvokeDBusServiceSpec('org.dbus.node.test', 'tcp:host=192.168.1.127,port=44444')
+    // await InvokeDBusServiceSpec('org.dbus.node.test', 'tcp:host=192.168.1.127,port=44444')
+    const dbus = await DBus.connect({busAddress: 'unix:path=/var/run/dbus/system_bus_socket'})
+    const service = await dbus.getService('org.freedesktop.login1')
+    const obj = await service.getObject('/org/freedesktop/login1')
+    const inter = await obj.getInterface('org.freedesktop.login1.Manager')
+    const fd = (await inter.method.Inhibit(
+        'shutdown:sleep',
+        'ControlServer',
+        'Mode management actions',
+        'delay'
+    )) as number
+    console.log('fd: ' + fd)
 })
