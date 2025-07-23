6dcf4b944c9c2c6993a2c81d4b412ffd77b4d23b
myq1991 <myq1991@gmail.com>
2025-07-23 19:01:25 +0800

    Debug

--- src/tests/Test.spec.ts
+++ src/tests/Test.spec.ts
@@ -27,7 +27,7 @@ setImmediate(async (): Promise<void> => {
     // dbus.addMatch('destination=""')
     // dbus.addMatch('\'\'')
     // dbus.addMatch('type=\'method_return\'')
-    dbus.addMatch('type=method_return,sender=:1.8')
+    dbus.addMatch('sender=:1.8')
     // console.log('Connect to DBus OK!',dbus.uniqueName)
     // const service = await dbus.getService('org.freedesktop.login1')
     // console.log('Get DBus service OK!')
