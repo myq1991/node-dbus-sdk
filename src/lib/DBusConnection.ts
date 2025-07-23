e73907c53415899852563f401fc5635080c099f9
myq1991 <myq1991@gmail.com>
2025-07-23 19:30:43 +0800

    Debug

--- src/lib/DBusConnection.ts
+++ src/lib/DBusConnection.ts
@@ -427,6 +427,7 @@ export class DBusConnection extends EventEmitter {
                         fieldsAndBody = stream.read(fieldsAndBodyLength)
                         if (!fieldsAndBody) break
                         state = false
+                        console.log(Buffer.concat([header,fieldsAndBody]))
                         const decMsg = DBusMessage.decode(header, fieldsAndBody, fieldsLength, bodyLength, advancedResponse, convertBigIntToNumber)
                         // if (decMsg.header.type === 2)
                         console.log(decMsg, header.length + fieldsAndBody.length, readableLength)
