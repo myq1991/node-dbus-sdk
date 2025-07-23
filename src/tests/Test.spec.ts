4ab56289da3a4b71a6a37e9562ad12071a93aaf6
myq1991 <myq1991@gmail.com>
2025-07-23 21:22:14 +0800

    fix(tests): remove unused DBus code and re-enable test specs

--- src/tests/Test.spec.ts
+++ src/tests/Test.spec.ts
@@ -1,6 +1,5 @@
 import {SetupLocalServiceSpec} from './examples/SetupLocalService.spec'
 import {InvokeDBusServiceSpec} from './examples/InvokeDBusService.spec'
-import {DBus} from '../NodeDBusSDK'
 
 /**
  * Executes test specifications for DBus service setup and invocation.
@@ -8,41 +7,10 @@ import {DBus} from '../NodeDBusSDK'
  * demonstrating both server and client interactions over a TCP connection.
  * The operations are scheduled to run immediately in the next event loop cycle.
  */
-// setImmediate(async (): Promise<void> => {
-//     // Set up a local DBus service with the specified name and TCP bus address
-//     await SetupLocalServiceSpec('org.dbus.node.test', 'tcp:host=192.168.1.127,port=44444')
-//
-//     // Invoke the DBus service as a client to test interactions with the local service
-//     await InvokeDBusServiceSpec('org.dbus.node.test', 'tcp:host=192.168.1.127,port=44444')
-// })
-
 setImmediate(async (): Promise<void> => {
     // Set up a local DBus service with the specified name and TCP bus address
-    // await SetupLocalServiceSpec('org.dbus.node.test', 'tcp:host=192.168.1.127,port=44444')
+    await SetupLocalServiceSpec('org.dbus.node.test', 'tcp:host=192.168.1.127,port=44444')
 
     // Invoke the DBus service as a client to test interactions with the local service
-    // await InvokeDBusServiceSpec('org.dbus.node.test', 'tcp:host=192.168.1.127,port=44444')
-    const dbus = await DBus.connect({busAddress: 'unix:path=/var/run/dbus/system_bus_socket'})
-    // dbus.addMatch(`destination=:${dbus.uniqueName}`)
-    // dbus.addMatch('destination=""')
-    // dbus.addMatch('\'\'')
-    // dbus.addMatch('type=\'method_return\'')
-    // dbus.addMatch('type=method_return')
-    // dbus.addMatch("type=method_return,sender=:1.8")
-    // dbus.addMatch('sender=:1.8')
-    console.log('Connect to DBus OK!',dbus.uniqueName)
-    const service = await dbus.getService('org.freedesktop.login1')
-    console.log('Get DBus service OK!')
-    const obj = await service.getObject('/org/freedesktop/login1')
-    console.log('Get DBus object OK!')
-    const inter = await obj.getInterface('org.freedesktop.login1.Manager')
-    console.log('Get DBus interface OK!')
-    const fd = (await inter.method.Inhibit(
-        'shutdown:sleep',
-        'ControlServer',
-        'Mode management actions',
-        'delay'
-    )) as number
-    console.log('Run Inhibit method done!')
-    console.log('fd: ' + fd)
+    await InvokeDBusServiceSpec('org.dbus.node.test', 'tcp:host=192.168.1.127,port=44444')
 })
