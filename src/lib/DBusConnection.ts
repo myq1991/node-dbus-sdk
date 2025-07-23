0105aea2eb08bd7f41e1b9474f33349062f900c2
myq1991 <myq1991@gmail.com>
2025-07-23 18:31:29 +0800

    Debug

--- src/lib/DBusConnection.ts
+++ src/lib/DBusConnection.ts
@@ -432,7 +432,8 @@ export class DBusConnection extends EventEmitter {
                         if (!fieldsAndBody) break
                         state = false
                         const decMsg = DBusMessage.decode(header, fieldsAndBody, fieldsLength, bodyLength, advancedResponse, convertBigIntToNumber)
-                        if (decMsg.header.type === 2) console.log(decMsg)
+                        // if (decMsg.header.type === 2)
+                            console.log(decMsg)
                         this.emit('message', decMsg)
                         // this.emit('message', DBusMessage.decode(header, fieldsAndBody, fieldsLength, bodyLength, advancedResponse, convertBigIntToNumber))
                     }
